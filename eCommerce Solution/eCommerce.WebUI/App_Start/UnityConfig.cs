782a8921825a935e0f42a2f140b348a15a237461
Sam Lehardi <slehardi@gmail.com>
2019-06-25 17:58:03 -0500

    Connected to SQL

--- eCommerce Solution/eCommerce.WebUI/App_Start/UnityConfig.cs
+++ eCommerce Solution/eCommerce.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using eCommerce.Core.Contracts;
 using eCommerce.Core.Models;
 using eCommerce.DataAccess.InMemory;
+using eCommerce.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace eCommerce.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
 
         }
     }
