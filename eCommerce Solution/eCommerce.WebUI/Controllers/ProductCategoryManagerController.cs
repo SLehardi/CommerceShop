9542bb0d29ff1463a676bbe4dc5f9dfd4184ede2
Sam Lehardi <slehardi@gmail.com>
2019-06-25 15:11:16 -0500

    Dependency Injection Added

--- eCommerce Solution/eCommerce.WebUI/Controllers/ProductCategoryManagerController.cs
+++ eCommerce Solution/eCommerce.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using eCommerce.Core.Contracts;
 using eCommerce.Core.Models;
 using eCommerce.DataAccess.InMemory;
 
@@ -10,11 +11,11 @@ namespace eCommerce.WebUI.Controllers
 {
     public class ProductCategoryManagerController : Controller
     {
-        InMemoryRepository<ProductCategory> context;
+        IRepository<ProductCategory> context;
 
-        public ProductCategoryManagerController()
+        public ProductCategoryManagerController(IRepository<ProductCategory> context)
         {
-            context = new InMemoryRepository<ProductCategory>();
+            this.context = context;
         }
 
         // GET: ProductManager
