b2f301f7fcc37bc5b015650479b9816cdec4c0a1
Sam Lehardi <slehardi@gmail.com>
2019-06-27 11:24:04 -0500

    Added basket UI

--- eCommerce Solution/eCommerce.WebUI/App_Start/UnityConfig.cs
+++ eCommerce Solution/eCommerce.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using eCommerce.Core.Contracts;
 using eCommerce.Core.Models;
 using eCommerce.DataAccess.InMemory;
 using eCommerce.DataAccess.SQL;
+using eCommerce.Services;
 using System;
 
 using Unity;
@@ -48,6 +49,9 @@ namespace eCommerce.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
 
         }
     }
